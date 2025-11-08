9077f3acb6f1e085080200b480d158550fe10dd1
Ibra Decode <ibradecode@gmail.com>
2025-11-08 15:12:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-<<<<<<< HEAD
-      base: '/',
-=======
       base: './',
->>>>>>> 849b86786f126b71b86727ce95a30529d96613d0
       server: {
         port: 3000,
         host: '0.0.0.0',
